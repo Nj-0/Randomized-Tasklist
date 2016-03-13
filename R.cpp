967fdb4085a3fda09ce654d161a151757cd60883
Naman Jain <nammanjain@gmail.com>
2016-03-13 17:41:24 +0530

    First Release !! Gets the work done :)

--- R.cpp
+++ R.cpp
@@ -1,33 +1,54 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
     vector<string> Tasks;
+    int randex;
+    srand((int)time(0));
+    string Task;
+    //=======================================================================================================RETRIEVE
+    ifstream input("Tasklist.txt");
+
+    while(getline(input, Task))
+        Tasks.push_back(Task);
+                input.close();
+    //=======================================================================================================
+
+
     while (1)
     {
-        cout << "1. Enter a Task " << endl;
-        cout << "2. List all Tasks. " << endl;
-        cout << "0. Exit." << endl;
-        //cout << "4. Delete a Task. " << endl;
+        //=======================================================================================================RANDOMIZER
+        if (Tasks.size()!=0)
+        {randex=rand()%Tasks.size();
+        cout << " The chosen TASK is '"<<Tasks[randex]<<"'" << endl;}
+        //=======================================================================================================SELECT
 
         char choice;
         cin >> choice;
 
-        if(choice == '1'){
+        if(choice == '1'){ //=======================================================================================================INSERT
             cout << "Please enter : ";
-            string Task;
             cin>> Task;
-            Tasks.push_back(Task);}
-
-        else if(choice == '2'){
-            cout << "You chose option #2." << endl; //Add Stuff Here
+            Tasks.push_back(Task);
             cout << "Listing Tasks. " << endl;
             for(int i = 0; i < Tasks.size(); i++)
                 cout << "Task # " << i + 1 << ": " << Tasks[i] << endl;}
 
-        else if(choice == '0')
-            break;
+        else if(choice == '0'){ //=======================================================================================================EXIT/STORE
+            ofstream f("Tasklist.txt");
+
+            if(f.is_open())
+                for(int i = 0; i<Tasks.size(); i++)
+                    f << Tasks[i]<<endl;
+                f.close();
+                break;}
+        else//=======================================================================================================HELP
+            cout << "1. Enter a Task " << endl;
+            cout << "0. Exit." << endl;
 
     }}
